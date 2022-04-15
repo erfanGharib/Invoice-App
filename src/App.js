eec28e4b6246327c549355af5fd8d34ece75297b
erfan gharib <erfangharib5@gmail.com>
2022-04-15 21:00:09 +0430

    start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './App.css';
+import './tailwind.css';
+
+const App = () => {
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './public/index.html',
+    './src/*.js',
+    './*.js',
+    './src/components/*.jsx'
+  ],
+  mode: 'jit',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
